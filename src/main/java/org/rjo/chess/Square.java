28ebd2d00c88f0c45043bf28882f58dc73686f55
rich oates <oates1@gmail.com>
2016-10-21 13:00:47 +0200

    changed Square to use array instead of map

--- src/main/java/org/rjo/chess/Square.java
+++ src/main/java/org/rjo/chess/Square.java
@@ -1,16 +1,12 @@
 package org.rjo.chess;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * An enum which maps between board squares and their bit positions (A1 == 0, H8 == 63)
- * and vice versa.
+ * An enum which maps between board squares and their bit positions (A1 == 0, H8 == 63) and vice versa.
  *
  * @author rich
  */
 public enum Square {
-   //@formatter:off
+	//@formatter:off
    a1( 0), a2( 8), a3(16), a4(24), a5(32), a6(40), a7(48), a8(56),
    b1( 1), b2( 9), b3(17), b4(25), b5(33), b6(41), b7(49), b8(57),
    c1( 2), c2(10), c3(18), c4(26), c5(34), c6(42), c7(50), c8(58),
@@ -21,133 +17,124 @@ public enum Square {
    h1( 7), h2(15), h3(23), h4(31), h5(39), h6(47), h7(55), h8(63);
    //@formatter:on
 
-   // look up map
-   private final static Map<Integer, Square> bitIndexToSquare = new HashMap<>(64);
-   static {
-      for (Square sq : Square.values()) {
-         bitIndexToSquare.put(sq.bitIndex(), sq);
-      }
-   }
+	// lookup int->Square
+	private final static Square[] bitIndexToSquare = new Square[64];
+	static {
+		for (Square sq : Square.values()) {
+			bitIndexToSquare[sq.bitIndex] = sq;
+		}
+	}
 
-   private int bitIndex;
-   private int rank;// Rank of this square (0..7)
-   private int file;// File of this square (0..7)
+	private int bitIndex;
+	private int rank;// Rank of this square (0..7)
+	private int file;// File of this square (0..7)
 
-   private Square(int bitIndex) {
-      this.bitIndex = bitIndex;
-      this.rank = bitIndex / 8;
-      this.file = bitIndex % 8;
-   }
+	private Square(int bitIndex) {
+		this.bitIndex = bitIndex;
+		this.rank = bitIndex / 8;
+		this.file = bitIndex % 8;
+	}
 
-   /**
-    * Rank of this square (0..7)
-    *
-    * @return
-    */
-   public int rank() {
-      return rank;
-   }
+	/**
+	 * Rank of this square (0..7)
+	 *
+	 * @return
+	 */
+	public int rank() {
+		return rank;
+	}
 
-   /**
-    * File of this square (0..7)
-    *
-    * @return
-    */
-   public int file() {
-      return file;
-   }
+	/**
+	 * File of this square (0..7)
+	 *
+	 * @return
+	 */
+	public int file() {
+		return file;
+	}
 
-   /**
-    * Delivers the bit posn of this square.
-    *
-    * @return the bit posn 0..63
-    */
-   public int bitIndex() {
-      return bitIndex;
-   }
+	/**
+	 * Delivers the bit posn of this square.
+	 *
+	 * @return the bit posn 0..63
+	 */
+	public int bitIndex() {
+		return bitIndex;
+	}
 
-   /**
-    * Gives the matching square to a bit posn.
-    *
-    * @param bitIndex
-    *           the required bit index (0..63)
-    * @return the matching square or null
-    */
-   public static Square fromBitIndex(int bitIndex) {
-      return bitIndexToSquare.get(bitIndex);
-   }
+	/**
+	 * Gives the matching square to a bit posn.
+	 *
+	 * @param bitIndex the required bit index (0..63)
+	 * @return the matching square or null
+	 */
+	public static Square fromBitIndex(int bitIndex) {
+		return bitIndexToSquare[bitIndex];
+	}
 
-   /**
-    * Maps a string "a5" to the corresponding enum value.
-    *
-    * @param coord
-    *           a string of length 2. Values A..H and 1..8.
-    * @return a Square or null.
-    */
-   public static Square fromString(String coord) {
-      if (coord.length() != 2) {
-         throw new IllegalArgumentException("expected a string of length 2. Got '" + coord + "'");
-      }
-      String fileStr = String.valueOf(coord.charAt(0)).toLowerCase();
-      char file = fileStr.charAt(0);
-      int rank = Character.digit(coord.charAt(1), 10);
-      if ((file < 'a') || (file > 'h')) {
-         throw new IllegalArgumentException("bad value for 'file': " + file);
-      }
-      if ((rank < 1) || (rank > 8)) {
-         throw new IllegalArgumentException("bad value for 'rank': " + rank);
-      }
-      return Square.fromRankAndFile(rank - 1, file - 'a');
-   }
+	/**
+	 * Maps a string "a5" to the corresponding enum value.
+	 *
+	 * @param coord a string of length 2. Values A..H and 1..8.
+	 * @return a Square or null.
+	 */
+	public static Square fromString(String coord) {
+		if (coord.length() != 2) {
+			throw new IllegalArgumentException("expected a string of length 2. Got '" + coord + "'");
+		}
+		String fileStr = String.valueOf(coord.charAt(0)).toLowerCase();
+		char file = fileStr.charAt(0);
+		int rank = Character.digit(coord.charAt(1), 10);
+		if ((file < 'a') || (file > 'h')) {
+			throw new IllegalArgumentException("bad value for 'file': " + file);
+		}
+		if ((rank < 1) || (rank > 8)) {
+			throw new IllegalArgumentException("bad value for 'rank': " + rank);
+		}
+		return Square.fromRankAndFile(rank - 1, file - 'a');
+	}
 
-   /**
-    * Maps from a rank/file mapping to a Square.
-    *
-    * @param rank
-    *           the rank (0..7)
-    * @param file
-    *           the file (0..7)
-    * @return the square
-    */
-   public static Square fromRankAndFile(int rank, int file) {
-      int bitPosn = (rank * 8) + (file);
-      return Square.fromBitIndex(bitPosn);
-   }
+	/**
+	 * Maps from a rank/file mapping to a Square.
+	 *
+	 * @param rank the rank (0..7)
+	 * @param file the file (0..7)
+	 * @return the square
+	 */
+	public static Square fromRankAndFile(int rank, int file) {
+		int bitPosn = (rank * 8) + (file);
+		return Square.fromBitIndex(bitPosn);
+	}
 
-   /**
-    * Returns the square where the pawn must be, given the enpassant square.
-    * e.g. given b6, returns b5.
-    *
-    * @param sq
-    *           start square. Must be either on the 6th rank (for a black move) or on the 3rd rank (for a white move).
-    * @return the square where the pawn moved to (on the 5th rank for black or the 4th rank for white).
-    */
-   public static Square findMoveFromEnpassantSquare(Square sq) {
-      if (sq.rank() == 5) {
-         return Square.fromRankAndFile(4, sq.file());
-      } else if (sq.rank() == 2) {
-         return Square.fromRankAndFile(3, sq.file());
-      } else {
-         throw new IllegalArgumentException("must specify an enpassant square, but got: " + sq);
-      }
-   }
+	/**
+	 * Returns the square where the pawn must be, given the enpassant square. e.g. given b6, returns b5.
+	 *
+	 * @param sq start square. Must be either on the 6th rank (for a black move) or on the 3rd rank (for a white move).
+	 * @return the square where the pawn moved to (on the 5th rank for black or the 4th rank for white).
+	 */
+	public static Square findMoveFromEnpassantSquare(Square sq) {
+		if (sq.rank() == 5) {
+			return Square.fromRankAndFile(4, sq.file());
+		} else if (sq.rank() == 2) {
+			return Square.fromRankAndFile(3, sq.file());
+		} else {
+			throw new IllegalArgumentException("must specify an enpassant square, but got: " + sq);
+		}
+	}
 
-   /**
-    * Returns the enpassant square, given the (pawn) move.
-    * e.g. given b5, returns b6. Or a4, returns a3.
-    *
-    * @param sq
-    *           the square where the pawn moved to (on the 5th rank for black or the 4th rank for white).
-    *
-    * @return enpassant square. Must be either on the 6th rank (for a black move) or on the 3rd rank (for a white move).
-    */
-   public static Square findEnpassantSquareFromMove(Square sq) {
-      if (sq.rank() == 4) {
-         return Square.fromRankAndFile(5, sq.file());
-      } else if (sq.rank() == 3) {
-         return Square.fromRankAndFile(2, sq.file());
-      } else {
-         throw new IllegalArgumentException("must specify a square on the 4th or 5th rank, but got: " + sq);
-      }
-   }
+	/**
+	 * Returns the enpassant square, given the (pawn) move. e.g. given b5, returns b6. Or a4, returns a3.
+	 *
+	 * @param sq the square where the pawn moved to (on the 5th rank for black or the 4th rank for white).
+	 * @return enpassant square. Must be either on the 6th rank (for a black move) or on the 3rd rank (for a white move).
+	 */
+	public static Square findEnpassantSquareFromMove(Square sq) {
+		if (sq.rank() == 4) {
+			return Square.fromRankAndFile(5, sq.file());
+		} else if (sq.rank() == 3) {
+			return Square.fromRankAndFile(2, sq.file());
+		} else {
+			throw new IllegalArgumentException("must specify a square on the 4th or 5th rank, but got: " + sq);
+		}
+	}
 }
