6a256d1a62dc9de1e0c7cf35b05a07a133563d3c
rich oates <oates1@gmail.com>
2017-07-09 17:18:59 +0200

    added debug move logging for Perft

--- src/main/java/org/rjo/chess/Perft.java
+++ src/main/java/org/rjo/chess/Perft.java
@@ -13,6 +13,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Starting point for 'Perft' tests.
  *
@@ -20,40 +23,45 @@ import java.util.Map;
  */
 public class Perft {
 
-	public static void main(String[] args) {
+	private static final Logger MOVE_LOGGER = LogManager.getLogger("MOVE-LOG");
+
+	public static void main(
+			String[] args) {
 		// see PerftTest::posn6ply5
-		// 5ply: 164.075.551 moves ( 489.246 ms) ( 335,4 moves/ms) (01.04.2015)
-		// 5ply: 164.075.551 moves ( 190.683 ms) ( 860,5 moves/ms) (03.04.2015)
-		// 5ply: 164.075.551 moves ( 164.298 ms) ( 998,6 moves/ms) (03.04.2015)
-		// 5ply: 164.075.551 moves ( 171.302 ms) ( 957,8 moves/ms) (regression)
-		// 5ply: 164.075.551 moves ( 142.830 ms) (1148,7 moves/ms)
-		// (updateStructures 100% incremental)
+		// 5ply: 164.075.551 moves
 		Game game = Fen.decode("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 		long start = System.currentTimeMillis();
 		Map<String, Integer> moveMap = findMoves(game.getPosition(), Colour.WHITE, 5);
 		long time = System.currentTimeMillis() - start;
 		int moves = Perft.countMoves(moveMap);
-		System.out.println(String.format(Locale.GERMANY, "5ply: %,12d moves (%,9d ms) (%7.1f moves/ms)", moves, time,
-				((moves * 1.0) / time)));
+		System.out
+				.println(String.format(Locale.GERMANY, "5ply: %,12d moves (%,9d ms) (%7.1f moves/ms)", moves, time, ((moves * 1.0) / time)));
+		if (moves != 164075551) {
+			System.out.println("ERROR: wrong number of moves");
+		}
 	}
 
 	/**
-	 * Find the number of possible moves at the given depth, starting at the current position given
-	 * by 'game'. I.e., for a depth of 2 and start colour white, all of black's moves will be
-	 * returned for each of the possible white moves. NB: Only leaf nodes are counted.
+	 * Find the number of possible moves at the given depth, starting at the current position given by 'game'. I.e., for a
+	 * depth of 2 and start colour white, all of black's moves will be returned for each of the possible white moves. NB:
+	 * Only leaf nodes are counted.
 	 *
 	 * @param posn a game position
 	 * @param sideToMove the starting colour
 	 * @param depth the required depth to search
-	 * @return a map containing all the start moves for the 'sideToMove' and for each map entry, a
-	 *         number representing how many leaf nodes there are from this starting move.
+	 * @return a map containing all the start moves for the 'sideToMove' and for each map entry, a number representing how
+	 *         many leaf nodes there are from this starting move.
 	 */
-	public static Map<String, Integer> findMoves(Position posn, Colour sideToMove, int depth) {
+	public static Map<String, Integer> findMoves(
+			Position posn,
+			Colour sideToMove,
+			int depth) {
 		if (depth < 1) {
 			throw new IllegalArgumentException("depth must be >= 1");
 		}
-		Map<String, Integer> moveMap = new HashMap<String, Integer>();
+		Map<String, Integer> moveMap = new HashMap<>();
 		for (Move move : posn.findMoves(sideToMove)) {
+			logMove(depth, move, posn);
 			Position posnAfterMove = posn.move(move);
 			int nbrMoves = findMovesInternal(posnAfterMove, Colour.oppositeColour(sideToMove), depth - 1);
 			moveMap.put(move.toString(), nbrMoves);
@@ -62,10 +70,10 @@ public class Perft {
 	}
 
 	/**
-	 * Helper routine to return the total number of moves found, given a map as returned from
-	 * findMoves.
+	 * Helper routine to return the total number of moves found, given a map as returned from findMoves.
 	 */
-	public static int countMoves(Map<String, Integer> moveMap) {
+	public static int countMoves(
+			Map<String, Integer> moveMap) {
 		int nbrMoves = 0;
 		for (String move : moveMap.keySet()) {
 			nbrMoves += moveMap.get(move);
@@ -74,45 +82,60 @@ public class Perft {
 	}
 
 	/**
-	 * Find the number of possible moves at the given depth, starting at the current position given
-	 * by 'game'. I.e., for a depth of 2 and start colour white, all of black's moves will be
-	 * returned for each of the possible white moves. NB: Only leaf nodes are counted.
+	 * Find the number of possible moves at the given depth, starting at the current position given by 'game'. I.e., for a
+	 * depth of 2 and start colour white, all of black's moves will be returned for each of the possible white moves. NB:
+	 * Only leaf nodes are counted.
 	 *
 	 * @param posn the game position
 	 * @param sideToMove the starting colour
 	 * @param depth the required depth to search
 	 * @return the total number of moves (leaf nodes) found from this position.
 	 */
-	private static int findMovesInternal(final Position posn, Colour sideToMove, int depth) {
+	private static int findMovesInternal(
+			final Position posn,
+			Colour sideToMove,
+			int depth) {
 		if (depth == 0) {
 			return 1;
 		}
 		int totalMoves = 0;
 		for (Move move : posn.findMoves(sideToMove)) {
+			logMove(depth, move, posn);
 			Position posnAfterMove = posn.move(move);
 			totalMoves += findMovesInternal(posnAfterMove, Colour.oppositeColour(sideToMove), depth - 1);
 		}
 		return totalMoves;
 	}
 
+	private static void logMove(
+			int depth,
+			Move move,
+			Position posn) {
+		if (MOVE_LOGGER.isDebugEnabled()) {
+			MOVE_LOGGER.debug(depth + " " + move + " " + Fen.encode(posn));
+		}
+	}
+
 	/**
-	 * Like {@link #findMoves(Game, Colour, int)} but with the option of storing the moves in a file.
-	 * Uses more memory than the other method and is therefore not recommended. <b>This is mainly for
-	 * PerftTests</b>. We return List of String instead of List of Move to try to conserve memory.
+	 * Like {@link #findMoves(Game, Colour, int)} but with the option of storing the moves in a file. Uses more memory than
+	 * the other method and is therefore not recommended. <b>This is mainly for PerftTests</b>. We return List of String
+	 * instead of List of Move to try to conserve memory.
 	 *
 	 * @param game the game
 	 * @param sideToMove the starting colour
 	 * @param depth the required depth to search
-	 * @return the list of possible moves at the given depth. The string representation is stored in
-	 *         order to save memory.
+	 * @return the list of possible moves at the given depth. The string representation is stored in order to save memory.
 	 * @throws IOException if can't write to temp file
 	 */
-	public static List<String> findMovesDebug(Game game, Colour sideToMove, int depth) throws IOException {
+	public static List<String> findMovesDebug(
+			Game game,
+			Colour sideToMove,
+			int depth) throws IOException {
 		File file = File.createTempFile("perft", null);
 		System.out.println("writing to " + file);
 		try (Writer debugWriter = new BufferedWriter(new FileWriter(file))) {
-			return findMovesInternalDebug(game.getPosition(), sideToMove, depth, new ArrayDeque<String>(1000),
-					new ArrayList<String>(500000), debugWriter);
+			return findMovesInternalDebug(game.getPosition(), sideToMove, depth, new ArrayDeque<String>(1000), new ArrayList<String>(500000),
+					debugWriter);
 		}
 	}
 
@@ -125,13 +148,17 @@ public class Perft {
 	 * @param movesSoFar for debugging purposes: the moves up to this point
 	 * @param totalMoves stores all moves found
 	 * @param debugWriter if not null, debug-info will be written to this file
-	 * @return the list of possible moves at the given depth. The string representation is stored in
-	 *         order to save memory.
+	 * @return the list of possible moves at the given depth. The string representation is stored in order to save memory.
 	 */
-	private static List<String> findMovesInternalDebug(Position posn, Colour sideToMove, int depth,
-			Deque<String> movesSoFar, List<String> totalMoves, Writer debugWriter) {
+	private static List<String> findMovesInternalDebug(
+			Position posn,
+			Colour sideToMove,
+			int depth,
+			Deque<String> movesSoFar,
+			List<String> totalMoves,
+			Writer debugWriter) {
 		if (depth == 0) {
-			return new ArrayList<String>();
+			return new ArrayList<>();
 		}
 		// movesAtThisLevel and movesSoFar are only used for "logging"
 		List<String> movesAtThisLevel = new ArrayList<>(10000);
@@ -141,8 +168,8 @@ public class Perft {
 			}
 			Position posnAfterMove = posn.move(move, debugWriter);
 
-			List<String> movesFromThisPosn = findMovesInternalDebug(posnAfterMove, Colour.oppositeColour(sideToMove),
-					depth - 1, movesSoFar, totalMoves, debugWriter);
+			List<String> movesFromThisPosn = findMovesInternalDebug(posnAfterMove, Colour.oppositeColour(sideToMove), depth - 1, movesSoFar,
+					totalMoves, debugWriter);
 			if (movesFromThisPosn.isEmpty()) {
 				totalMoves.add(move.toString());
 				if (debugWriter != null) {
